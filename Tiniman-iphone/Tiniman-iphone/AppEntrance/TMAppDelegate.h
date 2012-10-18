4e38f5ba04e4383535f744f443613df39fdf79bf
sunny <sunyuan1713@gmail.com>
2012-10-18 11:47:14 +0800

    整体目录结构修改

---
+++ Tiniman-iphone/Tiniman-iphone/AppEntrance/TMAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  TMAppDelegate.h
+//  Tiniman-iphone
+//
+//  Created by sunny on 12-10-18.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@class TMViewController;
+
+@interface TMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) TMViewController *viewController;
+
+@end
---
+++ Tiniman-iphone/Tiniman-iphone/AppEntrance/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tiniman-iphone
+//
+//  Created by sunny on 12-10-18.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TMAppDelegate class]));
+    }
+}
