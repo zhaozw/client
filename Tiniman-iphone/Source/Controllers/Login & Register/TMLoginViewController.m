1a9bd214994af5fb70314d25d49156b63963e6e7
sunny <sunyuan1713@gmail.com>
2012-10-28 01:02:34 +0800

    在login页面测试下头像的请求和缓存

--- Tiniman-iphone/Source/Controllers/Login & Register/TMLoginViewController.m
+++ Tiniman-iphone/Source/Controllers/Login & Register/TMLoginViewController.m
@@ -12,21 +12,33 @@
 
 @end
 
+#import "TMDataFacade.h"
+
 @implementation TMLoginViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    
+    //imageview
+    UIImageView* imageView = [[UIImageView alloc] initWithFrame:CGRectMake(100, 100, 50, 50)];
+    [self.view addSubview:imageView];
+    imageView.backgroundColor = [UIColor blueColor];
+    [imageView release];
+    
+    
+    //test avatar
+    NSURL* url = [NSURL URLWithString:@"http://www.baidu.com/img/baidu_sylogo1.gif"];
+    NSURL* url2 = [NSURL URLWithString:@"http://img.baidu.com/video/img/BD_video_logo.gif"];
+    
+    [[TMDataFacade facade] requestAvatarWithURL:url2 uid:@"uid123" timestamp:@"11a234" success:^(UIImage *avatarImage) {
+        imageView.image = avatarImage;
+    } fail:^(NSInteger error) {
+        NSLog(@"fail");
+    }];
+    
 }
 
 - (void)didReceiveMemoryWarning
