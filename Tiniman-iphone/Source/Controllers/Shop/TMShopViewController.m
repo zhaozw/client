eaa61569ed3c689afec7045b0baae4da6f010750
Cure <edjocure@gmail.com>
2012-11-02 11:06:26 +0800

    Initially tested: network interface for shop

--- Tiniman-iphone/Source/Controllers/Shop/TMShopViewController.m
+++ Tiniman-iphone/Source/Controllers/Shop/TMShopViewController.m
@@ -7,10 +7,25 @@
 //
 
 #import "TMShopViewController.h"
+#import "TMData.h"
+#import "MBProgressHUD.h"
+#import "TKAlertCenter.h"
+
+typedef enum {
+    TMShopTableViewTagProp = 100,
+    TMShopTableViewTagCoin
+} TMShopTableViewTag;
 
 @interface TMShopViewController ()
-@property (retain, nonatomic) IBOutlet UITableView *tableView;
+
+//@property (retain, nonatomic) IBOutlet UITableView *tableView;
 @property (retain, nonatomic) IBOutlet UIButton *flipButton;
+
+@property (retain, nonatomic) NSArray *propListArray;
+@property (retain, nonatomic) NSArray *coinListArray;
+@property (retain, nonatomic) IBOutlet UIView *tableContainerView;
+@property (retain, nonatomic) UITableView *propTableView;
+@property (retain, nonatomic) UITableView *coinTabelView;
 
 @end
 
@@ -28,7 +43,25 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+
+    self.coinListArray = [NSArray array];
+    self.propListArray = [NSArray array];
+    
+    self.coinTabelView = [[UITableView alloc] initWithFrame:CGRectMake(0.0f, 0.0f,_tableContainerView.bounds.size.width, _tableContainerView.bounds.size.height)
+                                                      style:UITableViewStylePlain];
+    _coinTabelView.delegate = self;
+    _coinTabelView.dataSource = self;
+    _coinTabelView.tag = TMShopTableViewTagCoin;
+    [_tableContainerView addSubview:_coinTabelView];
+    [_coinTabelView release];
+    
+    self.propTableView = [[UITableView alloc] initWithFrame:CGRectMake(0.0f, 0.0f,_tableContainerView.bounds.size.width, _tableContainerView.bounds.size.height)
+                                                      style:UITableViewStylePlain];
+    _propTableView.delegate = self;
+    _propTableView.dataSource = self;
+    _propTableView.tag = TMShopTableViewTagProp;
+    [_tableContainerView addSubview:_propTableView];
+    [_propTableView release];
 }
 
 - (void)didReceiveMemoryWarning
@@ -38,14 +71,19 @@
 }
 
 - (void)dealloc {
+    [_coinListArray release];
+    [_propListArray release];
+    
     [_flipButton release];
-    [_tableView release];
+//    [_tableView release];
+    [_tableContainerView release];
     [super dealloc];
 }
 
 - (void)viewDidUnload {
     [self setFlipButton:nil];
-    [self setTableView:nil];
+//    [self setTableView:nil];
+    [self setTableContainerView:nil];
     [super viewDidUnload];
 }
 
@@ -53,12 +91,26 @@
 {
     [super viewWillAppear:animated];
     
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.navigationController.view animated:animated];
+    hud.labelText = @"Loading...";
+    
+    [[TMDataFacade facade] requestShopListWithSuccessBlock:^(TMShopModel *shopModel) {
+        [hud hide:YES];
+        self.coinListArray = shopModel.coinPackages;
+        self.propListArray = shopModel.propPackages;
+        [_coinTabelView reloadData];
+        [_propTableView reloadData];
+    } fail:^(NSError *error) {
+        [hud hide:NO];
+        [[TKAlertCenter defaultCenter] postAlertWithMessage:error.localizedDescription image:[UIImage imageNamed:@"beer.png"]];
+    }];
+    
     if (_shopeType == TMShopTypeCoin) {
-//        _flipButton.titleLabel.text = @"Prop";
         [_flipButton setTitle:@"Prop" forState:UIControlStateNormal];
+//        _propTableView.hidden = YES;
     } else {
-//        _flipButton.titleLabel.text = @"Coin";
         [_flipButton setTitle:@"Coin" forState:UIControlStateNormal];
+//        _coinTabelView.hidden = YES;
     }
 }
 
@@ -71,17 +123,10 @@
 
 - (IBAction)flipToAnotherShop:(id)sender
 {
-//    if ([self.flipButton.titleLabel.text isEqualToString:@"Coin"]) {
-//        self.flipButton.titleLabel.text = @"Prop";
-//    } else {
-//        self.flipButton.titleLabel.text = @"Coin";
-//    }
     if (_shopeType == TMShopTypeProp) {
-//        _flipButton.titleLabel.text = @"Prop";
         [_flipButton setTitle:@"Prop" forState:UIControlStateNormal];
         _shopeType = TMShopTypeCoin;
     } else {
-//        _flipButton.titleLabel.text = @"Coin";
         [_flipButton setTitle:@"Coin" forState:UIControlStateNormal];
         _shopeType = TMShopTypeProp;
     }
@@ -96,19 +141,51 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 8;
+//    return 8;
+    if (tableView.tag == TMShopTableViewTagProp) {
+        return [_propListArray count];
+    } else {
+        return [_coinListArray count];
+    }
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 44.0f;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    
+    
+//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kTabelViewCellShop];
+    
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kTabelViewCellShop];
+    
+    if (!cell) {
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:kTabelViewCellShop] autorelease];
+    }
+    
+    
+    
+    if (TMShopTableViewTagProp == tableView.tag) {
+        TMShopPropPackageModel *propPackage = [_propListArray objectAtIndex:indexPath.row];
+        CLog(@"%@", propPackage);
+        cell.textLabel.text = propPackage.propPackageName;
+        cell.detailTextLabel.text = [NSString stringWithFormat:@"%d", propPackage.propNumber];
+    } else {
+        TMShopCoinPackageModel *coinPackage = [_coinListArray objectAtIndex:indexPath.row];
+        CLog(@"%@", coinPackage);
+        cell.textLabel.text = coinPackage.coinPackageName;
+        cell.detailTextLabel.text = [NSString stringWithFormat:@"%d", coinPackage.coinNumber];
+    }
     
     return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 @end
--- Tiniman-iphone/Source/Data/TMDataUtils.h
+++ Tiniman-iphone/Source/Data/TMDataUtils.h
@@ -17,7 +17,7 @@ if([(error) isKindOfClass:[NSError class]])\
 }
 
 //#define TMDataLocalHostURL [NSURL URLWithString:@"http://127.0.0.1:8000/"]
-#define TMDataLocalHostURL [NSURL URLWithString:@"http://192.168.1.108:8000/"]
+#define TMDataLocalHostURL [NSURL URLWithString:@"http://192.168.1.109:8000/"]
 
 #define TMDataServerBaseURL TMDataLocalHostURL
 
